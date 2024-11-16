7cfe7a12fd2758dbeaf8c3e5f93629f968669178
James Sharp <jamesrosssharp@gmail.com>
2024-11-16 20:55:58 +1100

    Added offset to randomize the crossover point wrt the clock edge

--- sim/design001/extract_results.py
+++ sim/design001/extract_results.py
@@ -27,6 +27,7 @@ temp = []
 power_min = []
 power_max = []
 power_av  = []
+pds = []
 
 for n, plot in enumerate(plots[1:]):
 
@@ -46,12 +47,10 @@ for n, plot in enumerate(plots[1:]):
     cross_time = plot['data']['time'][cross_point] 
     out_time = plot['data']['time'][out_point]
 
-    vdd.append(plot['data']['v(vdd)'][0])
-    vcm.append(plot['data']['v(inp)'][0])
-    temp.append(plot['data']['v(temperat)'][0])
+    pd = out_time - cross_time
 
     # Skip over startup transients
-    power = (-plot['data']['v(vdd)']*plot['data']['i(vvdd)'])[100:]
+    power = np.abs(plot['data']['v(vdd)']*plot['data']['i(vvdd)'])[100:]
     power_min.append(np.min(power))
     power_max.append(np.max(power))
     power_av.append(np.mean(power))
@@ -62,8 +61,12 @@ for n, plot in enumerate(plots[1:]):
 
     print("%d, " % n, end="")
     print("{:1.2g}, {:1.2g}, {:1.2g}, {:1.2g}, {:1.2g}, {:1.2g}, ".format(plot['data']['v(temperat)'][0], 
-        plot['data']['v(inp)'][0], plot['data']['v(vdd)'][0], cross_time, out_time, out_time -  cross_time), end="") 
-    if (out_time - cross_time < 30e-9):
+        plot['data']['v(inp)'][0], plot['data']['v(vdd)'][0], cross_time, out_time, pd), end="") 
+    if pd < 30e-9 and pd > 0:
+        vdd.append(plot['data']['v(vdd)'][0])
+        temp.append(plot['data']['v(temperat)'][0])
+        vcm.append(plot['data']['v(inp)'][0])
+        pds.append(pd)
         print("OK")
     else:
         print("Failed")
@@ -71,16 +74,20 @@ for n, plot in enumerate(plots[1:]):
         plot['data']['v(inp)'][0], plot['data']['v(vdd)'][0]))
 
 # Print common mode voltage range
-print("Common mode range: {:1.2g} - {:1.2g} V".format(np.min(np.array(vcm)), np.max(np.array(vcm))))
+print(" Working Common mode range: {:1.2g} - {:1.2g} V".format(np.min(np.array(vcm)), np.max(np.array(vcm))))
 
 # Print temperature range
-print("Temperature: {:.2f} - {:.2f} Deg C".format(np.min(np.array(temp)), np.max(np.array(temp))))
+print("Working Temperature range: {:.2f} - {:.2f} Deg C".format(np.min(np.array(temp)), np.max(np.array(temp))))
 
 # Print vdd range 
-print("Supply voltage range: {:1.2g} - {:1.2g} V".format(np.min(np.array(vdd)), np.max(np.array(vdd))))
+print("Working Supply voltage range: {:1.2g} - {:1.2g} V".format(np.min(np.array(vdd)), np.max(np.array(vdd))))
 
 # Print min, max and average power
 print("Power consumption: min {} max {} av {} W".format(np.min(np.array(power_min)), np.max(np.array(power_max)), np.mean(np.array(power_av))))
+
+print ("Average propagation delay: {} ns".format(np.mean(pds)))
+
+print()
 
 print("Failures: ")
 print(failures)
